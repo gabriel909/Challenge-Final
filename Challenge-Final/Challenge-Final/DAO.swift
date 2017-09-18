a4668818a623afe65f7c73887b9f31bb38999dbd
Luisa Mello <lu_mello93@hotmail.com>
2017-09-18 15:03:51 -0300

    Consertando conflitos

---
+++ Challenge-Final/Challenge-Final/DAO.swift
@@ -0,0 +1,58 @@
+//
+//  DAO.swift
+//  Challenge-Final
+//
+//  Created by Luisa Mello on 18/09/17.
+//  Copyright © 2017 Gabriel Oliveira. All rights reserved.
+//
+
+
+import Foundation
+
+class DAO {
+    
+    static let sharedDAO = DAO()
+    let token: String = ""
+    
+    private init() {
+        
+    }
+    
+    public func sendRequest(url: URL, parameters: [String:Any]? ,method: Methods,completion: @escaping ([String:Any]) -> Void) {
+        let session = URLSession.shared
+        var request = URLRequest(url: url)
+        request.httpMethod = method.rawValue
+        if parameters != nil {
+            do {
+                request.httpBody = try JSONSerialization.data(withJSONObject: parameters as Any, options: .prettyPrinted)
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        }
+        
+        
+        let task = session.dataTask(with: request) { (data, response, error) in
+            guard error == nil else {
+                return
+            }
+            
+            guard let data = data else {
+                return
+            }
+            
+            do {
+                if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: AnyObject] {
+                    completion(json)
+                }
+                
+            } catch let error {
+                print(error.localizedDescription)
+            }
+            
+            
+        }
+        task.resume()
+        
+    }
+    
+}
