d91d0cbacad9fba629bbd2324eea191e4722a22f
Luisa Mello <lu_mello93@hotmail.com>
2017-09-18 14:57:32 -0300

    SchoolListVC + gambi (var global)

--- Challenge-Final/Challenge-Final/Helper.swift
+++ Challenge-Final/Challenge-Final/Helper.swift
@@ -17,3 +17,56 @@ extension UINavigationBar {
         self.isTranslucent = true
     }
 }
+
+//Screen width and height
+let screenRect = UIScreen.main.bounds
+let width = screenRect.size.width
+let height = screenRect.size.height
+
+extension UIViewController {
+    //Hide the keyboard when user tap on the screen
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
+
+class SegueFromRight: UIStoryboardSegue {
+    override func perform() {
+        let src = self.source
+        let dst = self.destination
+        
+        src.view.superview?.insertSubview(dst.view, aboveSubview: src.view)
+        dst.view.transform = CGAffineTransform(translationX: src.view.frame.size.width, y: 0)
+        
+        UIView.animate(withDuration: 0.25, delay: 0.0, options: UIViewAnimationOptions.curveEaseInOut, animations: {
+            dst.view.transform = CGAffineTransform(translationX: 0, y: 0)
+            
+        }, completion: { finished in
+            src.present(dst, animated: false, completion: nil)
+            
+        })
+    }
+}
+
+class SegueFromLeft: UIStoryboardSegue {
+    override func perform() {
+        let src = self.source
+        let dst = self.destination
+        
+        src.view.superview?.insertSubview(dst.view, aboveSubview: src.view)
+        dst.view.transform = CGAffineTransform(translationX: -src.view.frame.size.width, y: 0)
+        
+        UIView.animate(withDuration: 0.25, delay: 0.0, options: UIViewAnimationOptions.curveEaseInOut, animations: {
+            dst.view.transform = CGAffineTransform(translationX: 0, y: 0)
+            
+        }, completion: { finished in
+            src.present(dst, animated: false, completion: nil)
+            
+        })
+    }
+}
---
+++ Challenge-Final/Challenge-Final/SchoolsTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  SchoolsTableViewCell.swift
+//  Challenge-Final
+//
+//  Created by Luisa Mello on 18/09/17.
+//  Copyright © 2017 Gabriel Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class SchoolsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var schoolName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
