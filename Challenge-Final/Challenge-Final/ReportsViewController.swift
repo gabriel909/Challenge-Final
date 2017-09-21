0b184d5a8c08c98317246adcca16ac0527730073
Luisa Mello <lu_mello93@hotmail.com>
2017-09-21 12:08:04 -0300

    Denuncias e avisos (em processo)

---
+++ Challenge-Final/Challenge-Final/ReportsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ReportsViewController.swift
+//  Challenge-Final
+//
+//  Created by Luisa Mello on 21/09/17.
+//  Copyright © 2017 Gabriel Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ReportsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
