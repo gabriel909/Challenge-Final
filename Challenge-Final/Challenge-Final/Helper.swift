f61caba05d575123e1037072ad0da0048c88208b
Luisa Mello <lu_mello93@hotmail.com>
2017-09-21 11:40:50 -0300

    MainVC

--- Challenge-Final/Challenge-Final/Helper.swift
+++ Challenge-Final/Challenge-Final/Helper.swift
@@ -70,3 +70,53 @@ class SegueFromLeft: UIStoryboardSegue {
         })
     }
 }
+
+extension UIView {
+    
+    @IBInspectable
+    var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+    
+    
+    @IBInspectable
+    var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable
+    var borderColor: UIColor? {
+        get {
+            let color = UIColor.init(CGColor: layer.borderColor!)
+            return color
+        }
+        set {
+            layer.borderColor = newValue?.CGColor
+        }
+    }
+    
+    @IBInspectable
+    var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+            layer.shadowColor = UIColor.blackColor().CGColor
+            layer.shadowOffset = CGSize(width: 0, height: 2)
+            layer.shadowOpacity = 0.4
+            layer.shadowRadius = shadowRadius
+        }
+    }
+    
+}
