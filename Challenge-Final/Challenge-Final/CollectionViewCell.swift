dbc4fa0a3824accb6696aceb62e038865a2ce3e0
Luisa Mello <lu_mello93@hotmail.com>
2017-09-26 13:14:17 -0300

    Colleciton View cagada e outras VCs

---
+++ Challenge-Final/Challenge-Final/CollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CollectionViewCell.swift
+//  Challenge-Final
+//
+//  Created by Luisa Mello on 26/09/17.
+//  Copyright © 2017 Gabriel Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var img: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
